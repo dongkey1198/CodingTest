c465b4193a82d060af4420ca0f1726325f06101f
dongkey1198 <51209390+dongkey1198@users.noreply.github.com>
2021-05-17 17:43:24 +0900

    Add files via upload

--- 520/bf8.js
+++ 520/bf8.js
@@ -11,56 +11,57 @@ rl.on('line', (line)=>{
 }).on('close', ()=>{
     const n = Number(input.shift());
     const arr = [];
+    const output = [];
     const visited = new Array(n).fill(false);
     let min = Infinity;
     
     for(let i = 0; i < input.length; i++){
         arr.push(input[i].split(' ').map(num => Number(num)));
     }
-    
-    dfs(0, 0);
-    
-    function dfs(idx, cnt){
 
+    dfs(0, 0);
+    console.log(min);
+
+    function dfs(idx, cnt){
         if(cnt === n/2){
-            calc();
+            // console.log(output);
+            // console.log(visited);
+            diff();
             return;
         }
-        for(let i = idx; i <= n; i++){
+
+        for(let i = idx; i < n; i++){
             if(!visited[i]){
+                if(output[output.length - 1] > i) continue;
                 visited[i] = true;
-                dfs(i + 1, cnt + 1);
+                output.push(i);
+                dfs(idx + 1, cnt + 1);
+                output.pop();
                 visited[i] = false;
             }
         }
     }
 
-    function calc(){
+    function diff(){
         let team_start = 0;
         let team_link = 0;
 
-        for(let i = 0; i < n - 1; i++){
-            for( let j = i +1; j < n; j++){
-                if(visited[i] && visited[j]){
-                        team_start += arr[i][j];
-                        team_start += arr[j][i];
-                } else if(!visited[i] && !visited[j]){
-                        team_link += arr[i][j];
-                        team_link += arr[j][i];
+        for(let i = 0; i < n-1; i++){
+            for(let j = i + 1; j < n; j++){
+                if(visited[i]===true && visited[j] === true){
+                    team_start += arr[i][j] + arr[j][i];
+                } else if(visited[i]===false && visited[j] === false) {
+                    team_link += arr[i][j] + arr[j][i];
                 }
             }
         }
-
-        let val = Math.abs(team_start - team_link);
-
+        let val = Math.abs(team_start-team_link);
         if(val === 0){
-            return min = 0;
+            min = 0;
+        } else{
+            min = Math.min(min, val);
         }
-
-        min = Math.min(min, val);
     }
 
-    console.log(min);
-    
     process.exit();
 });
